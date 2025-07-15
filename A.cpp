b85c3737a97f5b2a9c0f3276cf4fad739b4b4119
chong <chong@LAPTOP-GLQ9CIL3>
2025-07-15 23:26:33 +0800

    Add project files.

---
+++ A.cpp
@@ -0,0 +1,36 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector <string> appointment = { "10:30", "14:15", "16:00", "18:30" };
+
+
+	appointment.erase(appointment.begin() + 2);
+	appointment.insert(appointment.begin() + 1, "16:20");
+	appointment[2] = "16:20";
+
+
+	for (int i = 0; i < appointment.size(); i++) {
+		if (appointment[i] == "16:20") {
+			appointment.insert(appointment.begin() + i + 1, "17.10");
+			break;
+		}
+	}
+
+
+
+	for (int i = 0; i < appointment.size(); i++) {
+		if (appointment[i] == "10:30") {
+			appointment.erase(appointment.begin() + i);
+			break;
+		}
+	}
+
+	for (string app : appointment) {
+		cout << app << endl;
+	}
+	return 0;
+
+}
