f7358035de363f9baf08556308c1c31d96ecc59d
wenqi0401 <chongwq-wm24@student.tarc.edu.my>
2025-08-25 17:40:22 +0800

    Add event registration system structure

--- A.cpp
+++ A.cpp
@@ -1,41 +1,96 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
+#include <iomanip>
+#include <format>
 #include <vector>
+#include <regex>
+#include <sstream>
+#include <algorithm>
+#include<string>
+#include<cstring>
+#include<fstream>
+#include <map>
+
+
 using namespace std;
 
+struct EventRegistration {
+    string registrationID;
+    string manName;
+    string womanName;
+    string phone;
+    string email;
+    int numberOfGuests;
+    string packageType;
+    string specialRequests;
+};
 int main() {
-	vector <string> appointment = { "10:30", "14:15", "16:00", "18:30" };
+
+    vector<EventRegistration> registrations;
+
+    ifstream inputFile("registrations.txt");
+    if (!inputFile) {
+        cerr << "Error opening file." << endl;
+        return 1;
+    }
+    string line;
+    while (getline(inputFile, line)) {
+        stringstream ss(line);
+        EventRegistration reg;
+        getline(ss, reg.registrationID, ',');
+        getline(ss, reg.manName, ',');
+        getline(ss, reg.womanName, ',');
+        getline(ss, reg.phone, ',');
+        getline(ss, reg.email, ',');
+        string guestsStr;
+        getline(ss, guestsStr, ',');
+        reg.numberOfGuests = stoi(guestsStr);
+        getline(ss, reg.packageType, ',');
+        getline(ss, reg.specialRequests, ',');
+        registrations.push_back(reg);
+
+    }
+    inputFile.close();
+    cout << left << setw(15) << "Reg ID"
+        << setw;
 
 
-	appointment.erase(appointment.begin() + 2);
-	appointment.insert(appointment.begin() + 1, "16:20");
-	appointment[2] = "16:20";
 
-
-	for (int i = 0; i < appointment.size(); i++) {
-		if (appointment[i] == "16:20") {
-			appointment.insert(appointment.begin() + i + 1, "17.10");
-			break;
-		}
-	}
-
-// I try edit la /////////
-// hello i am jy
-	
-	for (int i = 0; i < appointment.size(); i++) {
-		if (appointment[i] == "10:30") {
-			appointment.erase(appointment.begin() + i);
-			break;
-		}
-	}
-	cout << "aaa";
-	cout << "aaa";
-	//aaaa
-
-	for (string app : appointment) {
-		cout << app << endl;
-		break;
-	}
 	return 0;
+
+}
+
+void displayMenu() {
+    cout << "Event Registration System" << endl;
+    cout << "1. View All Registrations" << endl;
+    cout << "2. Search Registration by ID" << endl;
+    cout << "3. Add New Registration" << endl;
+    cout << "4. Update Registration" << endl;
+    cout << "5. Delete Registration" << endl;
+    cout << "6. Exit" << endl;
+    cout << "Enter your choice: ";
+}
+
+void choice() {
+    int choice;
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+
+
+    case 2:
+
+    case 3:
+    case 4:
+
+
+    default:
+        break;
+    }
+}
+
+void addRegisteration() {
+    vector<EventRegistration> registrations;
+
 
 }
